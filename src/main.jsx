6912d426b6fdbf619c4072fe57046defe2f9bf99
davlatsultonov <davlatsultonov2001@gmail.com>
2023-08-15 07:05:02 +0500

    change vite config

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ErrorPage from './components/ErrorPage.jsx';
+import { ErrorPage } from './components/ErrorPage.jsx';
 import Product from './components/Product.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store/index.js';
 
+const productionPrefix = import.meta.env.MODE === 'production' ? '/store-project/' : '/'
+
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: `${productionPrefix}`,
     element: <App />,
-    errorElement: <ErrorPage />,
   },
   {
-    path: '/product/:id',
+    path: `${productionPrefix}product/:id`,
     element: <Product />,
   },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const isProduction = mode === 'production';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  return {
+    plugins: [react()],
+    base: isProduction ? '/store-project/' : '',
+  }
+});
