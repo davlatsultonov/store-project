a04f22571c84695299858f4477e95d3736a62193
davlatsultonov <davlatsultonov2001@gmail.com>
2023-08-16 10:41:42 +0500

    add active classes to categories elements

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -1,18 +1,26 @@
 import {capitalizeFirstLetter} from "../../helpers/helpers.js";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {fetchProducts} from "../../store/reducers/ActionCreator.js";
 import {setMaxProductPrice, setMinProductPrice} from "../../store/reducers/ProductsSlice.js";
+import {useLocation, useParams} from "react-router-dom";
+import {setCurrentCategory} from "../../store/reducers/CategoriesSlice.js";
 
 export const CategoryItem = ({ name }) => {
     const dispatch = useDispatch()
+    const { currentCategory } = useSelector(state => state.categoryReducer)
 
     const handleCategoryChange = () => {
         dispatch(fetchProducts(name))
         dispatch(setMinProductPrice())
         dispatch(setMaxProductPrice())
+        dispatch(setCurrentCategory(name))
     }
 
-    return (<div title={capitalizeFirstLetter(name)} onClick={handleCategoryChange} className='text-center whitespace-nowrap truncate cursor-pointer bg-blue-100 hover:bg-blue-200 text-blue-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300'>
+    const cls = []
+
+    return (<div title={capitalizeFirstLetter(name)} onClick={handleCategoryChange} className={
+        `text-center whitespace-nowrap truncate cursor-pointer ${ name === currentCategory || (currentCategory === '' && name.toLowerCase() === 'all') ? 'bg-blue-200' : 'bg-blue-white border border-blue-100' } hover:bg-blue-200 text-blue-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300`
+    }>
         { capitalizeFirstLetter(name) }
     </div>)
 }
--- src/store/reducers/CategoriesSlice.js
+++ src/store/reducers/CategoriesSlice.js
@@ -5,11 +5,17 @@ const initialState = {
     categories: [],
     isLoading: false,
     error: false,
+    currentCategory: ''
 }
 
 export const categorySlice = createSlice({
     name: 'category',
     initialState,
+    reducers: {
+        setCurrentCategory: (state, { payload }) => {
+            state.currentCategory = payload
+        }
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchCategories.fulfilled.type, (state, action) => {
             state.isLoading = false;
@@ -23,5 +29,7 @@ export const categorySlice = createSlice({
         })
     }
 })
+
+export const { setCurrentCategory } = categorySlice.actions
 
 export default categorySlice.reducer;
