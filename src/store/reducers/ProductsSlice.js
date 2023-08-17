f821a056248b00421cb2a86870e04f5f8d219fa7
davlatsultonov <davlatsultonov2001@gmail.com>
2023-08-17 22:47:40 +0500

    minor fix

--- src/store/reducers/ProductsSlice.js
+++ src/store/reducers/ProductsSlice.js
@@ -16,22 +16,16 @@ const initialState = {
 }
 
 const handleProductsFilter = (state) => {
-    return state.products.filter(product => {
+    return state.products.sort((a, b) => {
+        const el1 = calculateWithDiscount(a.price, a.discountPercentage);
+        const el2 = calculateWithDiscount(b.price, b.discountPercentage);
+        if (state.sortType === SORT_ELEMENTS.cheap) return el1 - el2
+        if (state.sortType === SORT_ELEMENTS.expensive) return el2 - el1
+        return b.rating - a.rating
+    }).filter(product => {
         const price = parseInt(product.price)
         if (state.selectedBrand === '') return price >= state.minProductPrice && price <= state.maxProductPrice
         else return price >= state.minProductPrice && price <= state.maxProductPrice && product.brand === state.selectedBrand
-    }).sort((a, b) => {
-        if (state.sortType === SORT_ELEMENTS.cheap) {
-            const el1 = calculateWithDiscount(a.price, a.discountPercentage);
-            const el2 = calculateWithDiscount(b.price, b.discountPercentage);
-            return el1 - el2
-        }
-        if (state.sortType === SORT_ELEMENTS.expensive) {
-            const el1 = calculateWithDiscount(a.price, a.discountPercentage);
-            const el2 = calculateWithDiscount(b.price, b.discountPercentage);
-            return el2 - el1
-        }
-        return b.rating - a.rating
     });
 }
 
@@ -68,12 +62,12 @@ export const productSlice = createSlice({
         }) => {
             const filteredBrands = new Set(products.map(product => product.brand))
             state.brands = Array.from(filteredBrands)
+            state.minProductPrice = findMinPrice(products);
+            state.maxProductPrice = findMaxPrice(products);
             state.isLoading = false;
             state.error = false;
             state.products = products;
             state.filteredProducts = handleProductsFilter(state);
-            state.minProductPrice = findMinPrice(products);
-            state.maxProductPrice = findMaxPrice(products);
         }).addCase(fetchProducts.pending.type, (state, action) => {
             state.isLoading = true;
         }).addCase(fetchProducts.rejected.type, (state, action) => {
