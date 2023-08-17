bcf699a5e9e413a099a5aa8cc2ec4364e03de59a
davlatsultonov <davlatsultonov2001@gmail.com>
2023-08-17 23:48:33 +0500

    minor fix

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -1,12 +1,16 @@
 import {capitalizeFirstLetter} from "../../helpers/helpers.js";
 import {useDispatch, useSelector} from "react-redux";
 import {setCurrentCategory} from "../../store/reducers/ProductsSlice.js";
+import {setCurrentPage} from "../../store/reducers/PaginationSlice.js";
 
 export const CategoryItem = ({ name }) => {
     const dispatch = useDispatch()
     const { currentCategory } = useSelector(state => state.productReducer)
 
-    const handleCategoryChange = () => dispatch(setCurrentCategory(name.toLowerCase() === 'all' ? '' : name))
+    const handleCategoryChange = () => {
+        dispatch(setCurrentPage(1))
+        dispatch(setCurrentCategory(name.toLowerCase() === 'all' ? '' : name))
+    }
 
     return (<div title={capitalizeFirstLetter(name)} onClick={handleCategoryChange} className={
         `text-center whitespace-nowrap truncate cursor-pointer ${ name === currentCategory || (currentCategory === '' && name.toLowerCase() === 'all') ? 'bg-blue-200' : 'bg-blue-white border border-blue-100' } hover:bg-blue-200 text-blue-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300`
